da860c00c737a5d27d705bf28b2eacd2645c250f
星灵之刃 <chenhao.w@gmail.com>
2022-02-14 17:36:23 +0800

    switch to polygon mainnet
    fxck mainnet is so slow i can't bear it

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,4 @@
-const contractAddress = '0xDc6ad8D06d2C093598207B47C3eEACd826ED866b';
-const chainId = 80001;
+const contractAddress = '0x153FCd155EE78E8A0069c9236D9a1BcFAa3a6E78';
+const chainId = 137;
 import { abi } from './abi/abi.js';
 export { contractAddress, chainId, abi };
